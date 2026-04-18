6c6bb52b2e6c844b1f643c2eb88d7a9c88fb7bd7
Ayushi Yadav <yayushi1030@gmail.com>
2026-04-19 01:52:10 +0530

    Added New Programs

---
+++ 30DAYSCODING/Day-18/assign_cookies.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int findContentChildren(vector<int>& g, vector<int>& s) {
+    sort(g.begin(), g.end());
+    sort(s.begin(), s.end());
+
+    int i = 0; 
+    int j = 0; 
+
+    while (i < g.size() && j < s.size()) {
+        if (s[j] >= g[i]) {
+            i++; 
+        }
+        j++;
+    }
+
+    return i;
+}
+
+int main() {
+    int n, m;
+
+    cout << "Enter number of children: ";
+    cin >> n;
+    vector<int> g(n);
+
+    cout << "Enter greed factors: ";
+    for (int i = 0; i < n; i++) {
+        cin >> g[i];
+    }
+
+    cout << "Enter number of cookies: ";
+    cin >> m;
+    vector<int> s(m);
+
+    cout << "Enter cookie sizes: ";
+    for (int i = 0; i < m; i++) {
+        cin >> s[i];
+    }
+
+    int result = findContentChildren(g, s);
+    cout << "Maximum satisfied children: " << result << endl;
+
+    return 0;
+}
---
+++ 30DAYSCODING/Day-18/fair_rations.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    for (int &x : a) cin >> x;
+
+    int count = 0;
+
+    for (int i = 0; i < n - 1; i++) {
+        if (a[i] % 2 != 0) {
+            a[i]++;       
+            a[i + 1]++;   
+            count += 2;
+        }
+    }
+
+    
+    if (a[n - 1] % 2 != 0) {
+        cout << "NO\n";
+    } else {
+        cout << count << "\n";
+    }
+
+    return 0;
+}
