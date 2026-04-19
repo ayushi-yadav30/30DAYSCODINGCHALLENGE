e9b9a086ae8bfd62cfeff67de50d729ec6f9c4c0
ayushi-yadav30 <yayushi1030@gmail.com>
2026-04-20 00:36:42 +0530

    UPLOADED NEW PROGRAM FILES

---
+++ 30DAYSCODING/Day-19/bulb_switch.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int bulbSwitch(int n) {
+    return (int)sqrt(n);
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    cout << bulbSwitch(n) << endl;
+
+    return 0;
+}
---
+++ 30DAYSCODING/Day-19/super_digit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    long long n;
+    printf("Enter a number: ");
+    scanf("%lld", &n);
+    
+    if (n == 0) {
+        printf("Super Digit: 0\n");
+    } else {
+        int result = n % 9;
+        printf("Super Digit: %d\n", (result == 0) ? 9 : result);
+    }
+    return 0;
+}
