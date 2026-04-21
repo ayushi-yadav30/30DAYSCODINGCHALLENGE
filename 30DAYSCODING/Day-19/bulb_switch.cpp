4fb71098d4533d3f94c8dc350cdf3b2f6687e015
Ayushi Yadav <yayushi1030@gmail.com>
2026-04-21 20:12:32 +0530

    Add files via upload

---
+++ 30DAYSCODING/Day-19/bulb_switch.cpp
@@ -0,0 +1,34 @@
+<<<<<<< HEAD
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int bulbSwitch(int n) {
+    return (int)sqrt(n);
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    cout << bulbSwitch(n) << endl;
+
+    return 0;
+=======
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int bulbSwitch(int n) {
+    return (int)sqrt(n);
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    cout << bulbSwitch(n) << endl;
+
+    return 0;
+>>>>>>> f2b6bca6b793a238bc096a552986d002e0848071
+}
---
+++ 30DAYSCODING/Day-20/reverse_tri.cpp
@@ -0,0 +1,26 @@
+<<<<<<< HEAD
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j;
+    for(i=0;i<6;i++){
+        for(j=i+1;j>0;j--){
+            cout<<j<<" ";
+    }
+        cout<<endl;
+    }
+return 0;
+=======
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j;
+    for(i=0;i<6;i++){
+        for(j=i+1;j>0;j--){
+            cout<<j<<" ";
+    }
+        cout<<endl;
+    }
+return 0;
+>>>>>>> f2b6bca6b793a238bc096a552986d002e0848071
+}
