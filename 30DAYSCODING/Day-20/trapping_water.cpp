f284f2b99e6e3e6eeb331c19326938235a1f5bf2
Ayushi Yadav <yayushi1030@gmail.com>
2026-04-22 22:54:18 +0530

    Update trapping_water.cpp

--- 30DAYSCODING/Day-20/trapping_water.cpp
+++ 30DAYSCODING/Day-20/trapping_water.cpp
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #include<iostream>
 #include<vector>
 using namespace std;
@@ -43,52 +43,4 @@ int trap(vector<int>& height) {
 
     return 0;
 
-    return 0;
-=======
-#include<iostream>
-#include<vector>
-using namespace std;
-int trap(vector<int>& height) {
-        int left = 0, right = height.size() - 1;
-        int leftMax = 0, rightMax = 0;
-        int water = 0;
-
-        while (left < right) {
-            if (height[left] < height[right]) {
-                if (height[left] >= leftMax) {
-                    leftMax = height[left];
-                } else {
-                    water += leftMax - height[left];
-                }
-                left++;
-            } else {
-                if (height[right] >= rightMax) {
-                    rightMax = height[right];
-                } else {
-                    water += rightMax - height[right];
-                }
-                right--;
-            }
-        }
-        return water;
-    }
-
-    int main(){
-        int n;
-    cout << "Enter number of elements: ";
-    cin >> n;
-
-    vector<int> height(n);
-    cout << "Enter heights: ";
-    for (int i = 0; i < n; i++) {
-        cin >> height[i];
-    }
-
-    int result = trap(height);
-    cout << "Trapped Water: " << result << endl;
-
-    return 0;
-
-    return 0;
->>>>>>> f2b6bca6b793a238bc096a552986d002e0848071
     }
