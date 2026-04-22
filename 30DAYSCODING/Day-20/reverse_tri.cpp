bedc07ea2a5bd2a0b24e3718a4623bf78b919a4d
Ayushi Yadav <yayushi1030@gmail.com>
2026-04-22 22:53:31 +0530

    Update reverse_tri.cpp

--- 30DAYSCODING/Day-20/reverse_tri.cpp
+++ 30DAYSCODING/Day-20/reverse_tri.cpp
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #include<iostream>
 using namespace std;
 int main(){
@@ -10,17 +10,4 @@ int main(){
         cout<<endl;
     }
 return 0;
-=======
-#include<iostream>
-using namespace std;
-int main(){
-    int i,j;
-    for(i=0;i<6;i++){
-        for(j=i+1;j>0;j--){
-            cout<<j<<" ";
-    }
-        cout<<endl;
-    }
-return 0;
->>>>>>> f2b6bca6b793a238bc096a552986d002e0848071
 }
