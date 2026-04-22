76cb2814610fa5aa0a80b5db8ed22b1f965c9b52
Ayushi Yadav <yayushi1030@gmail.com>
2026-04-22 22:52:41 +0530

    Update count_one.cpp

--- 30DAYSCODING/Day-20/count_one.cpp
+++ 30DAYSCODING/Day-20/count_one.cpp
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #include<iostream>
 using namespace std;
 int countDigitOne(int n){
@@ -31,38 +31,5 @@ int main(){
     
     cout<<"Count of '1' :"<<countDigitOne(N);
     return 0;
-=======
-#include<iostream>
-using namespace std;
-int countDigitOne(int n){
-    long long count=0;
 
-    for(long long p=0; p<=n; p*=10){
-long long last=n/(p*10);
-            long long middle=(n/p)%10;
-            long long first=n%p;
-
-            if(middle==0){
-                count+=last*p;
-                }
-                else if(middle==1){
-                    count+= last*p + first + 1; 
-                }
-                else{
-                    count+=(last+1) * p;
-                }
-                            
-        }
-        return count;
-
-    
-}
-int main(){
-    long long N;
-    cout<<"Enter the number:"<<endl;
-    cin>>N;
-    
-    cout<<"Count of '1' :"<<countDigitOne(N);
-    return 0;
->>>>>>> f2b6bca6b793a238bc096a552986d002e0848071
 }
