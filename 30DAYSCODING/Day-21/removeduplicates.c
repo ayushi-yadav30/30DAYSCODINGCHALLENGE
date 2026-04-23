07460272503c4a5de37e7c4797471309e2ab06de
Ayushi Yadav <yayushi1030@gmail.com>
2026-04-23 21:20:17 +0530

    "REVISION PROBLEMS ADDED"

---
+++ 30DAYSCODING/Day-21/removeduplicates.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int removeDuplicates(int arr[], int n) {
+    if (n == 0) return 0;
+
+    int j = 0; 
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] != arr[j]) {
+            j++;
+            arr[j] = arr[i];
+        }
+    }
+    return j + 1; 
+}
+
+int main() {
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter sorted elements: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int newSize = removeDuplicates(arr, n);
+
+    printf("Array after removing duplicates: ");
+    for (int i = 0; i < newSize; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
